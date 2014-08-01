b6a42d0f717d233f5e7f42e542cd9b8f14923a4e
Alexey <om.alex@hotmail.com>
2014-08-02 03:29:43 +1200

    4

--- app/js/directives/directives.js
+++ app/js/directives/directives.js
@@ -2,6 +2,7 @@ angular.module('components', [])
         .directive('dateBox', function() {
             return {
                 restrict: 'E',
+                replace: true,
                 scope: {
                     name: '='
                 },
@@ -11,6 +12,7 @@ angular.module('components', [])
         .directive('habitDay', function() {
             return {
                 restrict: 'E',
+                replace: true,
                 scope: {
                     day: '='
                 },
@@ -43,42 +45,3 @@ angular.module('components', [])
            //     template: "<div>LOGIIIIIIIIIIN</div>"
             };
         });
-
-/////////////////////////////////////////////////////
-    app.directive('myDraggable', ['$document', function($document) {
-      return function(scope, element, attr) {
-        var startX = 0, startY = 0, x = 0, y = 0;
-
-        element.css({
-         position: 'relative',
-         border: '1px solid red',
-         backgroundColor: 'lightgrey',
-         cursor: 'pointer'
-        });
-
-        element.on('mousedown', function(event) {
-          // Prevent default dragging of selected content
-          event.preventDefault();
-          startX = event.pageX - x;
-          startY = event.pageY - y;
-          $document.on('mousemove', mousemove);
-          $document.on('mouseup', mouseup);
-        });
-
-        function mousemove(event) {
-          y = event.pageY - startY;
-          x = event.pageX - startX;
-          element.css({
-            top: y + 'px',
-            left:  x + 'px'
-          });
-        }
-
-        function mouseup() {
-          $document.off('mousemove', mousemove);
-          $document.off('mouseup', mouseup);
-        }
-      };
-    }]);
-////////////////////////////////////////////////////
-
