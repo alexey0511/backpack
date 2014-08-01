8a797e9f6ec48d1c7c29c9ef92491ac1382344fa
Alexey <om.alex@hotmail.com>
2014-08-02 11:07:59 +1200

    add http

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
-//var express = require("express");
-//var logfmt = require("logfmt");
-//var app = express();
-
-//app.use(logfmt.requestLogger());
-//app.use(express.static('app'));
-//app.listen(process.env.PORT);
-
-var express = require('express');
-var http = require('http');
+var express = require("express");
+var logfmt = require("logfmt");
 var app = express();
 
-app.set('port', process.env.PORT || 80);
+app.use(logfmt.requestLogger());
+app.use(express.static('app'));
+app.listen(process.env.PORT);
 
-//DEFINE ALL ROUTES
-app.get('/', routes.dashboard.index);
-app.post('/*', function(request, response) {
-  response.redirect('/');
+server.post('/*', function(req, res){
+
+  res.sendfile('index.html');
 });
-
-//SETUP HTTP LISTENER
-
-server = http.createServer(app);
