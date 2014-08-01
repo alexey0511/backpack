6f0a3ccd2573de07979ce0bc0a971e36586950af
Alexey <om.alex@hotmail.com>
2014-08-02 10:43:42 +1200

    ssdf

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-//var app = require('express')();
-//app.use(express.static('app'));
-//app.listen(process.env.PORT);
-
-// web.js
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
@@ -11,11 +6,18 @@ app.use(logfmt.requestLogger());
 app.use(express.static('app'));
 app.listen(process.env.PORT);
 
-// app.get('/', function(req, res) {
-//   res.send('Hello World!');
-// });
-// 
-// var port = Number(process.env.PORT || 5000);
-// app.listen(port, function() {
-//   console.log("Listening on " + port);
-// });
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.set('port', process.env.PORT || 80);
+
+//DEFINE ALL ROUTES
+app.get('/', routes.dashboard.index);
+app.post('/*', function(request, response) {
+  response.redirect('/');
+});
+
+//SETUP HTTP LISTENER
+
+server = http.createServer(app);
