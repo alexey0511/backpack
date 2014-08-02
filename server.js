2e0ae0470bf276bf82b3f3af84ab2c63e736ec4a
Alexey <om.alex@hotmail.com>
2014-08-02 14:21:42 +1200

    sdf

--- server.js
+++ server.js
@@ -14,10 +14,18 @@
 
 
 // var server = http.createServer(app);
-var http = require("http");
+// web.js
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
