f779796ad9a60e3c377aecfc46cbf66a37c8607a
Alexey <om.alex@hotmail.com>
2014-08-02 13:49:48 +1200

    sdf

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
-app.set('port', process.env.PORT || 80);
+// var express = require("express");
+// var logfmt = require("logfmt");
+// var app = express();
+// app.set('port', process.env.PORT || 80);
 
-app.use(logfmt.requestLogger());
-app.use(express.static('app'));
-app.listen(process.env.PORT);
+// app.use(logfmt.requestLogger());
+// app.use(express.static('app'));
+// app.listen(process.env.PORT);
 
-app.post('/*', function(req, res){
-  res.sendfile('index.html');
-});
+// app.post('/*', function(req, res){
+  // res.sendfile('index.html');
+// });
 
 
 
-//var server = http.createServer(app);
+// var server = http.createServer(app);
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
