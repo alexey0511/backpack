5c23e6cbad7e9682b61b4db0b0e426eb3bcc8ee0
Alexey <om.alex@hotmail.com>
2014-11-19 21:12:24 +0700

    renewed

--- server.js
+++ server.js
@@ -1,27 +1,9 @@
-// var express = require("express");
-// var logfmt = require("logfmt");
-// var app = express();
-// app.set('port', process.env.PORT || 80);
-
-// app.use(logfmt.requestLogger());
-// app.use(express.static('app'));
-// app.listen(process.env.PORT);
-
-// app.post('/*', function(req, res){
-  // res.sendfile('index.html');
-// });
-
-
-
-// var server = http.createServer(app);
-// web.js
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
-
 app.use(logfmt.requestLogger());
-app.use(express.static('./app'));
-//app.listen(process.env.PORT);
+app.use(express.static('../'));
+app.listen(process.env.PORT);
 
 app.get('/', function(req, res) {
   res.send('Hello World! PORT: ' +process.env.PORT);
@@ -33,5 +15,5 @@ app.listen(port, function() {
 });
 
  app.post('/*', function(req, res){
-   res.sendfile('./app/index1.html');
+   res.sendfile('./app/index1.html')
  });
