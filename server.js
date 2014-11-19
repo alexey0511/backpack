d22b96ac2b0aa856569db97ba6cd1031cc925a92
om.alex@hotmail.com <om.alex@hotmail.com>
2014-11-19 14:09:58 +0000

    8

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 app.use(logfmt.requestLogger());
-app.use(express.static('./app'));
+app.use(express.static('../'));
 app.listen(process.env.PORT);
 
 app.get('/', function(req, res) {
@@ -15,5 +15,5 @@ app.listen(port, function() {
 });
 
  app.post('/*', function(req, res){
-   res.sendfile('index1.html')
+   res.sendfile('./app/index1.html')
  });
