a3965b1ba92d736afbc4757b668db3f486e9a2f8
Alexey <om.alex@hotmail.com>
2014-11-20 12:22:57 +0700

    a

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 app.use(logfmt.requestLogger());
-app.use(express.static('../app'));
+app.use(express.static('../'));
 app.listen(process.env.PORT);
-
-app.get('/*', function(req, res) {
-});
-
 
  app.post('/*', function(req, res){
    res.sendfile('app/index1.html')
