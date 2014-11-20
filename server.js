0c9fa33aad809a44af1f8d51bd02607c304f02e7
Alexey <om.alex@hotmail.com>
2014-11-20 12:24:56 +0700

    a

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ app.use(logfmt.requestLogger());
 app.use(express.static('../'));
 app.listen(process.env.PORT);
 
+app.get('/*', function(req, res) {
+   res.sendfile('app/services.js')
+
+});
+
+
  app.post('/*', function(req, res){
    res.sendfile('app/index1.html')
  });
