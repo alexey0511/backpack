ff7be47901d1f848ea3dd61c541923be50e59d05
Alexey <om.alex@hotmail.com>
2014-11-20 12:17:54 +0700

    a

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 app.use(logfmt.requestLogger());
-app.use(express.static('../'));
+app.use(express.static('../app'));
 app.listen(process.env.PORT);
 
 app.get('/*', function(req, res) {
