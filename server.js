3aa0008885513a678492f4fef523076a7b2e39af
Alexey <om.alex@hotmail.com>
2014-11-20 14:02:04 +0700

    a

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ app.use(logfmt.requestLogger());
 app.use(express.static('/app'));
 app.listen(process.env.PORT || 5000);
 
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+ app.post('/*', function(req, res){
+   res.sendfile('index.html');
+ });
